bc346d617f53edc13841ca982e68f3f8bdf98f52
henok <henycave@gmail.com>
2023-04-10 00:14:26 +0300

    finished the task details screen

--- lib/screens/task_detail.dart
+++ lib/screens/task_detail.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:action_slider/action_slider.dart';
 import '../model/task.dart';
 
 class TaskDetail extends StatefulWidget {
@@ -20,6 +20,7 @@ class _TaskDetailState extends State<TaskDetail> {
         color: const Color(0xffFEF970),
         child: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -35,6 +36,77 @@ class _TaskDetailState extends State<TaskDetail> {
                     backgroundColor: Color(0xff111C2F),
                   )
                 ],
+              ),
+              const SizedBox(height: 20,),
+              OutlinedButton(
+                onPressed: null,
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0,)
+                  )),
+                ),
+                child: const Text("Sweet Home", style: TextStyle(color: Color(0xff343C13)),),
+              ),
+              const SizedBox(height: 20,),
+              const Text("Grocery \nShopping", style: TextStyle(fontSize: 80),),
+              const SizedBox(height: 25,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text("Time Left"),
+                  Text("Assignee"),
+                ],
+              ),
+              const SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text("2h 45m", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),),
+                      Text("Dec 12,2022"),
+                    ],
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage("images/profile.jpeg"),
+                    radius: 30.0,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30,),
+              const Text("Additional Description"),
+              const SizedBox(height: 10,),
+              const Text("We Have to Buy some fresh bread, fruit and vegetables."
+                  " supply of Water is running out.", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),),
+              const SizedBox(height: 30,),
+              const Text("Created"),
+              const SizedBox(height: 10,),
+              Row(
+                children: const [
+                  Text("Dec 10, by Matt", style: TextStyle(fontWeight: FontWeight.w400, fontSize: 17),),
+                  SizedBox(width: 5,),
+                  CircleAvatar(
+                    backgroundImage: AssetImage("images/profile.jpeg"),
+                    radius: 15.0,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 60,),
+              Align(
+                alignment: Alignment.center,
+                child: ActionSlider.standard(
+                  height: 70,
+                  toggleColor: Colors.white,
+                    icon: const Icon(Icons.check),
+                    child: const Text('Set as Done', style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w500),),
+                    backgroundColor: const Color(0xff111C2F),
+                    action: (controller) async {
+                      controller.loading(); //starts loading animation
+                      await Future.delayed(const Duration(seconds: 3));
+                      controller.success(); //starts success animation
+                    }
+                ),
               )
             ],
           ),
